c3e41b4aec193c04b92d8e1f59a5d13ef6f0bfac
rip_super <150400664+rip-super@users.noreply.github.com>
2025-12-18 15:38:29 -0600

    update server.js

--- server.js
+++ server.js
@@ -107,7 +107,8 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () =>
     console.log(`Server listening on http://localhost:${PORT}`)
 );
+
